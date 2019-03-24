5d8d01743817d4133987a691e31b68066647e668
Zhe Lu <lzanita09@gmail.com>
2019-03-23 19:05:07 -0700

    Add tests for email scheme query params

--- ifttt-sdk-android/src/test/java/com/ifttt/ui/ButtonApiHelperTest.java
+++ ifttt-sdk-android/src/test/java/com/ifttt/ui/ButtonApiHelperTest.java
@@ -2,6 +2,9 @@ package com.ifttt.ui;
 
 import android.net.Uri;
 import com.ifttt.Connection;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -20,8 +23,8 @@ public final class ButtonApiHelperTest {
 
     @Test
     public void testRequiredFields() {
-        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", "abc@efg.com", "", "auth_code",
-                null);
+        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", Collections.emptyList(),
+                "abc@efg.com", "", "auth_code", null);
 
         assertThat(uri.getQueryParameter("sdk_return_to")).isEqualTo("http://redirect");
         assertThat(uri.getQueryParameter("email")).isEqualTo("abc@efg.com");
@@ -30,45 +33,63 @@ public final class ButtonApiHelperTest {
 
     @Test
     public void testInviteCode() {
-        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", "abc@efg.com", "", "auth_code",
-                "abcd");
+        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", Collections.emptyList(),
+                "abc@efg.com", "", "auth_code", "abcd");
         assertThat(uri.getQueryParameter("invite_code")).isEqualTo("abcd");
     }
 
     @Test
     public void testOAuthCode() {
-        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", "abc@efg.com", "", "auth_code",
-                "abcd");
+        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", Collections.emptyList(),
+                "abc@efg.com", "", "auth_code", "abcd");
         assertThat(uri.getQueryParameter("code")).isNull();
 
-        Uri uri1 = ButtonApiHelper.getEmbedUri(connection, CreateAccount, "http://redirect", "abc@efg.com", "",
-                "auth_code", "abcd");
+        Uri uri1 = ButtonApiHelper.getEmbedUri(connection, CreateAccount, "http://redirect", Collections.emptyList(),
+                "abc@efg.com", "", "auth_code", "abcd");
         assertThat(uri1.getQueryParameter("code")).isEqualTo("auth_code");
 
-        Uri uri2 = ButtonApiHelper.getEmbedUri(connection, Login, "http://redirect", "abc@efg.com", "", "auth_code",
-                "abcd");
+        Uri uri2 = ButtonApiHelper.getEmbedUri(connection, Login, "http://redirect", Collections.emptyList(),
+                "abc@efg.com", "", "auth_code", "abcd");
         assertThat(uri2.getQueryParameter("code")).isEqualTo("auth_code");
     }
 
     @Test
     public void testServiceAuthentication() {
-        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", "abc@efg.com", "", "auth_code",
-                "abcd");
+        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", Collections.emptyList(),
+                "abc@efg.com", "", "auth_code", "abcd");
         assertThat(uri.getQueryParameter("skip_sdk_redirect")).isNull();
 
-        Uri uri1 = ButtonApiHelper.getEmbedUri(connection, ServiceAuthentication, "http://redirect", "abc@efg.com", "",
-                "auth_code", "abcd");
+        Uri uri1 = ButtonApiHelper.getEmbedUri(connection, ServiceAuthentication, "http://redirect",
+                Collections.emptyList(), "abc@efg.com", "", "auth_code", "abcd");
         assertThat(uri1.getQueryParameter("skip_sdk_redirect")).isEqualTo("true");
     }
 
     @Test
     public void testSdkCreateAccount() {
-        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", "abc@efg.com", "", "auth_code",
-                "abcd");
+        Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, "http://redirect", Collections.emptyList(),
+                "abc@efg.com", "", "auth_code", "abcd");
         assertThat(uri.getQueryParameter("sdk_create_account")).isNull();
 
-        Uri uri1 = ButtonApiHelper.getEmbedUri(connection, CreateAccount, "http://redirect", "abc@efg.com", "",
-                "auth_code", "abcd");
+        Uri uri1 = ButtonApiHelper.getEmbedUri(connection, CreateAccount, "http://redirect", Collections.emptyList(),
+                "abc@efg.com", "", "auth_code", "abcd");
         assertThat(uri1.getQueryParameter("sdk_create_account")).isEqualTo("true");
+    }
+
+    @Test
+    public void testEmailAppsDetectorWhenLogin() {
+        Uri uri = ButtonApiHelper.getEmbedUri(connection, Login, "http://redirect", Arrays.asList("a", "b"),
+                "abc@efg.com", "", "auth_code", "abcd");
+        List<String> params = uri.getQueryParameters("available_email_app_schemes[]");
+        assertThat(params).hasSize(2);
+        assertThat(params.get(0)).isEqualTo("a");
+        assertThat(params.get(1)).isEqualTo("b");
+    }
+
+    @Test
+    public void testEmailAppsDetectorWhenCreateAccount() {
+        Uri uri = ButtonApiHelper.getEmbedUri(connection, CreateAccount, "http://redirect", Arrays.asList("a", "b"),
+                "abc@efg.com", "", "auth_code", "abcd");
+        List<String> params = uri.getQueryParameters("available_email_app_schemes[]");
+        assertThat(params).hasSize(0);
     }
 }
