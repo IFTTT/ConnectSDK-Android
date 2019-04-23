3776c92be4785b4ea3eeae32523a1c6e20576ee5
Zhe Lu <lzanita09@gmail.com>
2019-04-23 15:15:13 -0700

    Expose value props and cover images
    https://ifttt-dev.atlassian.net/browse/IFTTT-1745

---
+++ connect-button/src/main/java/com/ifttt/ValueProposition.java
@@ -0,0 +1,51 @@
+package com.ifttt;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import com.squareup.moshi.Json;
+
+/**
+ * Value proposition data structure, including the icon url and the description.
+ */
+public final class ValueProposition implements Parcelable {
+    @Json(name = "icon_url") final String iconUrl;
+    final String description;
+
+    public ValueProposition(String iconUrl, String description) {
+        this.iconUrl = iconUrl;
+        this.description = description;
+    }
+
+    protected ValueProposition(Parcel in) {
+        iconUrl = in.readString();
+        description = in.readString();
+    }
+
+    public static final Creator<ValueProposition> CREATOR = new Creator<ValueProposition>() {
+        @Override
+        public ValueProposition createFromParcel(Parcel in) {
+            return new ValueProposition(in);
+        }
+
+        @Override
+        public ValueProposition[] newArray(int size) {
+            return new ValueProposition[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(iconUrl);
+        dest.writeString(description);
+    }
+
+    @Override
+    public String toString() {
+        return "ValueProposition{" + "iconUrl='" + iconUrl + '\'' + ", description='" + description + '\'' + '}';
+    }
+}
