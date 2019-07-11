bd08453fe7c25e4a903f80829e4c08c150921b4c
Zhe Lu <lzanita09@gmail.com>
2019-07-11 14:49:16 -0700

    Stop loading UI if suggested email is invalid (#116)
    * Stop loading UI if suggested email is invalid
    
    Instead of crashing the host app, we take a more graceful approach to deal with invalid emails on runtime: we will not render the UI if the email is invalid, but users can still stay in the app, but won't be able to use the ConnectButton.
    
    An exception is that if the user has the app install, we bypass the email validation, because we will redirect users to the app instead of presenting email field.
    
    * Remove log
    
    * Revert to use the original connection

--- connect-button/src/main/java/com/ifttt/connect/ui/ConnectButton.java
+++ connect-button/src/main/java/com/ifttt/connect/ui/ConnectButton.java
@@ -11,6 +11,7 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -94,6 +95,16 @@ public class ConnectButton extends FrameLayout implements LifecycleOwner {
      * @param configuration Configuration object that helps set up the Connect Button.
      */
     public void setup(Configuration configuration) {
+        if (ButtonUiHelper.isEmailInvalid(configuration.suggestedUserEmail) && !ButtonUiHelper.isIftttInstalled(
+                getContext().getPackageManager())) {
+            connectButton.setVisibility(View.GONE);
+            loadingView.setVisibility(View.GONE);
+            Log.e(ConnectButton.class.getSimpleName(), configuration.suggestedUserEmail + " is invalid.");
+            return;
+        }
+
+        connectButton.setVisibility(View.VISIBLE);
+        loadingView.setVisibility(View.VISIBLE);
         ConnectionApiClient clientToUse;
         if (configuration.connectionApiClient == null) {
             if (API_CLIENT == null) {
@@ -331,10 +342,6 @@ public class ConnectButton extends FrameLayout implements LifecycleOwner {
              */
             public static Builder withConnection(Connection connection, String suggestedUserEmail,
                     CredentialsProvider credentialsProvider, Uri connectRedirectUri) {
-                if (ButtonUiHelper.isEmailInvalid(suggestedUserEmail)) {
-                    throw new IllegalStateException(suggestedUserEmail + " is not a valid email address.");
-                }
-
                 Builder builder = new Builder(suggestedUserEmail, credentialsProvider, connectRedirectUri);
                 builder.connection = connection;
                 return builder;
@@ -355,10 +362,6 @@ public class ConnectButton extends FrameLayout implements LifecycleOwner {
              */
             public static Builder withConnectionId(String connectionId, String suggestedUserEmail,
                     CredentialsProvider credentialsProvider, Uri connectRedirectUri) {
-                if (ButtonUiHelper.isEmailInvalid(suggestedUserEmail)) {
-                    throw new IllegalStateException(suggestedUserEmail + " is not a valid email address.");
-                }
-
                 Builder builder = new Builder(suggestedUserEmail, credentialsProvider, connectRedirectUri);
                 builder.connectionId = connectionId;
                 return builder;
