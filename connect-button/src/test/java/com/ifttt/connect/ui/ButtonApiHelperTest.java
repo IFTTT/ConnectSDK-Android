2a3727d597f1ce6285e2d1e740ef1ed550654cf1
Priya Shah <priya@ifttt.com>
2020-06-17 02:01:23 -0700

    Apply gitignore

--- connect-button/src/test/java/com/ifttt/connect/ui/ButtonApiHelperTest.java
+++ connect-button/src/test/java/com/ifttt/connect/ui/ButtonApiHelperTest.java
@@ -25,7 +25,7 @@ public final class ButtonApiHelperTest {
     @Test
     public void testRequiredFields() {
         Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, redirectUri, Collections.emptyList(), "abc@efg.com",
-                null, "", "auth_code", null, false, "en"
+                null, "", "auth_code", null, false
         );
 
         assertThat(uri.getQueryParameter("sdk_return_to")).isEqualTo("http://redirect");
@@ -36,7 +36,7 @@ public final class ButtonApiHelperTest {
     @Test
     public void testInviteCode() {
         Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, redirectUri, Collections.emptyList(), "abc@efg.com",
-                null, "", "auth_code", "abcd", false, "en"
+                null, "", "auth_code", "abcd", false
         );
         assertThat(uri.getQueryParameter("invite_code")).isEqualTo("abcd");
     }
@@ -44,17 +44,17 @@ public final class ButtonApiHelperTest {
     @Test
     public void testOAuthCode() {
         Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, redirectUri, Collections.emptyList(), "abc@efg.com",
-                null, "", "auth_code", "abcd", false, "en"
+                null, "", "auth_code", "abcd", false
         );
         assertThat(uri.getQueryParameter("code")).isNull();
 
         Uri uri1 = ButtonApiHelper.getEmbedUri(connection, CreateAccount, redirectUri, Collections.emptyList(),
-                "abc@efg.com", null, "", "auth_code", "abcd", false, "en"
+                "abc@efg.com", null, "", "auth_code", "abcd", false
         );
         assertThat(uri1.getQueryParameter("code")).isEqualTo("auth_code");
 
         Uri uri2 = ButtonApiHelper.getEmbedUri(connection, Login, redirectUri, Collections.emptyList(), "abc@efg.com",
-                null, "", "auth_code", "abcd", false, "en"
+                null, "", "auth_code", "abcd", false
         );
         assertThat(uri2.getQueryParameter("code")).isEqualTo("auth_code");
     }
@@ -62,12 +62,12 @@ public final class ButtonApiHelperTest {
     @Test
     public void testSdkCreateAccount() {
         Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, redirectUri, Collections.emptyList(), "abc@efg.com",
-                null, "", "auth_code", "abcd", false, "en"
+                null, "", "auth_code", "abcd", false
         );
         assertThat(uri.getQueryParameter("sdk_create_account")).isNull();
 
         Uri uri1 = ButtonApiHelper.getEmbedUri(connection, CreateAccount, redirectUri, Collections.emptyList(),
-                "abc@efg.com", null, "", "auth_code", "abcd", false, "en"
+                "abc@efg.com", null, "", "auth_code", "abcd", false
         );
         assertThat(uri1.getQueryParameter("sdk_create_account")).isEqualTo("true");
     }
@@ -75,7 +75,7 @@ public final class ButtonApiHelperTest {
     @Test
     public void testUsername() {
         Uri uri = ButtonApiHelper.getEmbedUri(connection, Initial, redirectUri, Collections.emptyList(), "abc@efg.com",
-                "user_name", "", "auth_code", "abcd", false, "en"
+                "user_name", "", "auth_code", "abcd", false
         );
         assertThat(uri.getQueryParameter("username")).isEqualTo("user_name");
         assertThat(uri.getQueryParameter("email")).isNull();
@@ -84,7 +84,7 @@ public final class ButtonApiHelperTest {
     @Test
     public void testEmailAppsDetectorWhenLogin() {
         Uri uri = ButtonApiHelper.getEmbedUri(connection, Login, redirectUri, Arrays.asList("a", "b"), "abc@efg.com",
-                null, "", "auth_code", "abcd", false, "en"
+                null, "", "auth_code", "abcd", false
         );
         List<String> params = uri.getQueryParameters("available_email_app_schemes[]");
         assertThat(params).hasSize(2);
@@ -95,7 +95,7 @@ public final class ButtonApiHelperTest {
     @Test
     public void testEmailAppsDetectorWhenCreateAccount() {
         Uri uri = ButtonApiHelper.getEmbedUri(connection, CreateAccount, redirectUri, Arrays.asList("a", "b"),
-                "abc@efg.com", null, "", "auth_code", "abcd", false, "en"
+                "abc@efg.com", null, "", "auth_code", "abcd", false
         );
         List<String> params = uri.getQueryParameters("available_email_app_schemes[]");
         assertThat(params).hasSize(0);
@@ -104,7 +104,7 @@ public final class ButtonApiHelperTest {
     @Test
     public void testSkipConfigurationFlag() {
         Uri uri = ButtonApiHelper.getEmbedUri(connection, CreateAccount, redirectUri, Arrays.asList("a", "b"),
-            "abc@efg.com", null, "", "auth_code", "abcd", true, "en"
+            "abc@efg.com", null, "", "auth_code", "abcd", true
         );
 
         assertThat(uri.getQueryParameter("skip_config")).isEqualTo("true");
