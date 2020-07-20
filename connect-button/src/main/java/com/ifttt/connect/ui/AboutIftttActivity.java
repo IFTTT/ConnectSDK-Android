aba9ffa8d965d3958fea8d4dbbfb610850e54461
Zhe Lu <lzanita09@gmail.com>
2020-07-20 11:18:37 -0700

    Prefer ApplicationContext for resources if locales differ (#185)
    In the case that ApplicationContext's Locale and Activity's Locale are different, prefer using ApplicationContext to get string resources.

--- connect-button/src/main/java/com/ifttt/connect/ui/AboutIftttActivity.java
+++ connect-button/src/main/java/com/ifttt/connect/ui/AboutIftttActivity.java
@@ -3,7 +3,9 @@ package com.ifttt.connect.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
@@ -20,6 +22,7 @@ import androidx.core.content.res.ResourcesCompat;
 import com.ifttt.connect.R;
 import com.ifttt.connect.api.Connection;
 import com.ifttt.connect.api.Service;
+import java.util.Locale;
 
 import static com.ifttt.connect.ui.ButtonApiHelper.redirectToConnection;
 import static com.ifttt.connect.ui.ButtonApiHelper.redirectToService;
@@ -69,56 +72,10 @@ public final class AboutIftttActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        TextView title = findViewById(R.id.ifttt_about_title);
-        String aboutTitleString = getString(R.string.about_ifttt_connects_x_to_y, secondaryService.name, primaryService.name);
-        CharSequence replacedWithIftttLogo = ButtonUiHelper.replaceKeyWithImage(title, aboutTitleString, "IFTTT",
-                ContextCompat.getDrawable(this, R.drawable.ic_ifttt_logo_white));
-        Typeface bold = ResourcesCompat.getFont(this, R.font.avenir_next_ltpro_bold);
-        Spannable highlightServiceNames = new SpannableString(replacedWithIftttLogo);
-        int secondaryServiceNameStart = aboutTitleString.indexOf(secondaryService.name);
-        int secondaryServiceNameEnd = secondaryServiceNameStart + secondaryService.name.length();
-        int primaryServiceNameStart = aboutTitleString.indexOf(primaryService.name);
-        int primaryServiceNameEnd = primaryServiceNameStart + primaryService.name.length();
-        highlightServiceNames.setSpan(new AvenirTypefaceSpan(bold), secondaryServiceNameStart, secondaryServiceNameEnd,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        highlightServiceNames.setSpan(new AvenirTypefaceSpan(bold), primaryServiceNameStart, primaryServiceNameEnd,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        title.setText(highlightServiceNames);
-        title.setOnClickListener(v -> {
-            Intent intent = redirectToConnection(this, connection.id);
-            if (intent == null) {
-                return;
-            }
-
-            analyticsManager.trackUiClick(AnalyticsObject.CONNECTION_NAME, AnalyticsLocation.fromConnectButtonWithId(connection.id));
-            startActivity(intent);
-        });
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setNavigationOnClickListener(v -> finish());
 
-        // Set up links to terms of use and privacy policy.
-        TextView termsAndPrivacy = findViewById(R.id.term_and_privacy);
-
-        String content = getString(R.string.about_ifttt_privacy_and_terms);
-        String termPrivacyAndTerms = getString(R.string.term_privacy_and_terms);
-
-        int termPrivacyAndTermsIndex = content.indexOf(termPrivacyAndTerms);
-
-        SpannableString spanContent = new SpannableString(content);
-        if (termPrivacyAndTermsIndex != -1) {
-            spanContent.setSpan(new UnderlineSpan(), termPrivacyAndTermsIndex,
-                    termPrivacyAndTermsIndex + termPrivacyAndTerms.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-        termsAndPrivacy.setText(spanContent);
-
-        Intent redirectToTermsIntent = redirectToTerms(this);
-        if (redirectToTermsIntent != null) {
-            termsAndPrivacy.setOnClickListener(v -> {
-                startActivity(redirectToTermsIntent);
-                analyticsManager.trackUiClick(AnalyticsObject.PRIVACY_POLICY, AnalyticsLocation.fromConnectButtonWithId(connection.id));
-            });
-        }
+        renderLocalizedItems(primaryService, secondaryService, connection);
 
         View manageConnectionView = findViewById(R.id.ifttt_manage_connection);
         View googlePlayView = findViewById(R.id.google_play_link);
@@ -154,9 +111,97 @@ public final class AboutIftttActivity extends AppCompatActivity {
         analyticsManager.trackUiImpression(AnalyticsObject.CONNECT_INFORMATION_MODAL, AnalyticsLocation.fromConnectButtonWithId(connection.id));
     }
 
+    private void renderLocalizedItems(Service primaryService, Service secondaryService, Connection connection) {
+        TextView title = findViewById(R.id.ifttt_about_title);
+        String aboutTitleString = getLocalizedResources().getString(R.string.about_ifttt_connects_x_to_y, secondaryService.name, primaryService.name);
+        CharSequence replacedWithIftttLogo = ButtonUiHelper.replaceKeyWithImage(title, aboutTitleString, "IFTTT",
+            ContextCompat.getDrawable(this, R.drawable.ic_ifttt_logo_white));
+        Typeface bold = ResourcesCompat.getFont(this, R.font.avenir_next_ltpro_bold);
+        Spannable highlightServiceNames = new SpannableString(replacedWithIftttLogo);
+        int secondaryServiceNameStart = aboutTitleString.indexOf(secondaryService.name);
+        int secondaryServiceNameEnd = secondaryServiceNameStart + secondaryService.name.length();
+        int primaryServiceNameStart = aboutTitleString.indexOf(primaryService.name);
+        int primaryServiceNameEnd = primaryServiceNameStart + primaryService.name.length();
+        highlightServiceNames.setSpan(new AvenirTypefaceSpan(bold), secondaryServiceNameStart, secondaryServiceNameEnd,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        highlightServiceNames.setSpan(new AvenirTypefaceSpan(bold), primaryServiceNameStart, primaryServiceNameEnd,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        title.setText(highlightServiceNames);
+        title.setOnClickListener(v -> {
+            Intent intent = redirectToConnection(this, connection.id);
+            if (intent == null) {
+                return;
+            }
+
+            analyticsManager.trackUiClick(AnalyticsObject.CONNECTION_NAME, AnalyticsLocation.fromConnectButtonWithId(connection.id));
+            startActivity(intent);
+        });
+
+        TextView firstItem = findViewById(R.id.ifttt_about_1);
+        firstItem.setText(getLocalizedResources().getString(R.string.about_ifttt_unlock_new_features));
+
+        TextView secondItem = findViewById(R.id.ifttt_about_2);
+        secondItem.setText(getLocalizedResources().getString(R.string.about_ifttt_control_which_services_access_data));
+
+        TextView thirdItem = findViewById(R.id.ifttt_about_3);
+        thirdItem.setText(getLocalizedResources().getString(R.string.about_ifttt_protect_and_monitor_data));
+
+        TextView fourthItem = findViewById(R.id.ifttt_about_4);
+        fourthItem.setText(getLocalizedResources().getString(R.string.about_ifttt_unplug_any_time));
+
+        TextView manageButton = findViewById(R.id.ifttt_manage_connection);
+        manageButton.setText(getLocalizedResources().getString(R.string.manage));
+
+        // Set up links to terms of use and privacy policy.
+        TextView termsAndPrivacy = findViewById(R.id.term_and_privacy);
+
+        String content = getLocalizedResources().getString(R.string.about_ifttt_privacy_and_terms);
+        String termPrivacyAndTerms = getLocalizedResources().getString(R.string.term_privacy_and_terms);
+
+        int termPrivacyAndTermsIndex = content.indexOf(termPrivacyAndTerms);
+
+        SpannableString spanContent = new SpannableString(content);
+        if (termPrivacyAndTermsIndex != -1) {
+            spanContent.setSpan(new UnderlineSpan(), termPrivacyAndTermsIndex,
+                termPrivacyAndTermsIndex + termPrivacyAndTerms.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        termsAndPrivacy.setText(spanContent);
+
+        Intent redirectToTermsIntent = redirectToTerms(this);
+        if (redirectToTermsIntent != null) {
+            termsAndPrivacy.setOnClickListener(v -> {
+                startActivity(redirectToTermsIntent);
+                analyticsManager.trackUiClick(AnalyticsObject.PRIVACY_POLICY, AnalyticsLocation.fromConnectButtonWithId(connection.id));
+            });
+        }
+    }
+
+    private Resources getLocalizedResources() {
+        Locale applicationLocale = getLocaleFromContext(getApplicationContext());
+        Locale activityLocale = getLocaleFromContext(this);
+
+        // Prioritize ApplicationContext's locale.
+        if (activityLocale.equals(applicationLocale)) {
+            return this.getResources();
+        } else {
+            return getApplicationContext().getResources();
+        }
+    }
+
     private static final String EXTRA_CONNECTION = "extra_connection";
 
     public static Intent intent(Context context, Connection connection) {
         return new Intent(context, AboutIftttActivity.class).putExtra(EXTRA_CONNECTION, connection);
+    }
+
+    private static Locale getLocaleFromContext(Context context) {
+        Locale locale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            locale = context.getResources().getConfiguration().getLocales().get(0);
+        } else {
+            locale = context.getResources().getConfiguration().locale;
+        }
+
+        return locale;
     }
 }
