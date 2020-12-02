8fc563f721dfbb4e07263fc77d42e8c53b220f5f
Zhe Lu <lzanita09@gmail.com>
2020-12-01 19:27:06 -0800

    Add tests for CacheUserTokenProvider (#212)

---
+++ connect-location/src/test/java/com/ifttt/location/CacheUserTokenProviderTest.java
@@ -0,0 +1,57 @@
+package com.ifttt.location;
+
+import android.content.Context;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public final class CacheUserTokenProviderTest {
+
+    private Context context;
+
+    @Before
+    public void setUp() throws Exception {
+        context = InstrumentationRegistry.getInstrumentation().getContext();
+    }
+
+    @Test
+    public void shouldPreferDelegate() {
+        UserTokenCache cache = new UserTokenCache(context);
+        cache.store("cached");
+        CacheUserTokenProvider provider = new CacheUserTokenProvider(cache, () -> "delegate");
+
+        assertThat(provider.getUserToken()).isEqualTo("delegate");
+    }
+
+    @Test
+    public void shouldReturnCachedForNullDelegate() {
+        UserTokenCache cache = new UserTokenCache(context);
+        cache.store("cached");
+        CacheUserTokenProvider provider = new CacheUserTokenProvider(cache, null);
+
+        assertThat(provider.getUserToken()).isEqualTo("cached");
+    }
+
+    @Test
+    public void shouldCacheNonNullToken() {
+        UserTokenCache cache = new UserTokenCache(context);
+        CacheUserTokenProvider provider = new CacheUserTokenProvider(cache, () -> "delegate");
+        provider.getUserToken();
+
+        assertThat(cache.get()).isEqualTo("delegate");
+    }
+
+    @Test
+    public void shouldNotCacheNullToken() {
+        UserTokenCache cache = new UserTokenCache(context);
+        CacheUserTokenProvider provider = new CacheUserTokenProvider(cache, () -> null);
+        provider.getUserToken();
+
+        assertThat(cache.get()).isNull();
+    }
+}
