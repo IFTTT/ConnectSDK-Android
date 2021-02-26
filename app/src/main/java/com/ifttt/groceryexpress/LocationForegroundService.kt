8ccd6f86d93cd951a1dfe1462397eb2b6ad4f1dd
Zhe Lu <lzanita09@gmail.com>
2021-02-26 10:47:20 -0800

    Implement device location polling (#221)

--- app/src/main/java/com/ifttt/groceryexpress/LocationForegroundService.kt
+++ app/src/main/java/com/ifttt/groceryexpress/LocationForegroundService.kt
@@ -1,16 +1,35 @@
 package com.ifttt.groceryexpress
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.O
+import android.os.Bundle
 import android.os.IBinder
+import android.os.Looper
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.ifttt.location.ConnectLocation
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Example implementation of a foreground service: a persistent service constantly running on the background with a
@@ -21,7 +40,19 @@ import androidx.core.app.NotificationManagerCompat
  *
  * More details see https://developer.android.com/about/versions/oreo/background-location-limits.
  */
-class LocationForegroundService : Service() {
+class LocationForegroundService : Service(), CoroutineScope {
+
+    private lateinit var parentJob: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + parentJob
+
+    override fun onCreate() {
+        super.onCreate()
+
+        parentJob = SupervisorJob()
+    }
+
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -45,10 +76,59 @@ class LocationForegroundService : Service() {
         }
 
         startForeground(FOREGROUND_SERVICE_NOTIFICATION_ID, notification)
+
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        // Poll LocationManager for the device's current location, and update ConnectLocation
+        // with any missed geo-fence events from Awareness API.
+        launch(Dispatchers.IO) {
+            while (ContextCompat.checkSelfPermission(
+                    this@LocationForegroundService,
+                    ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                locationManager.requestSingleUpdate(Criteria().apply {
+                    accuracy = Criteria.ACCURACY_FINE
+                }, object : LocationListener {
+                    override fun onLocationChanged(location: Location) {
+                        ConnectLocation.getInstance().reportEvent(
+                            this@LocationForegroundService,
+                            location.latitude,
+                            location.longitude,
+                            null
+                        )
+                    }
+
+                    override fun onStatusChanged(
+                        provider: String?,
+                        status: Int,
+                        extras: Bundle?
+                    ) {
+                        // No-op
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+                        // No-op
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+                        // No-op
+                    }
+                }, Looper.getMainLooper())
+
+                delay(LOCATION_POLLING_DELAY)
+            }
+        }
+
         return super.onStartCommand(intent, flags, startId)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
+    }
+
     companion object {
+        private const val LOCATION_POLLING_DELAY = 2 * 60L * 1000L
         private const val FOREGROUND_SERVICE_NOTIFICATION_ID = 1001
         private const val NOTIFICATION_CHANNEL_ID = "location_foreground_service"
 
